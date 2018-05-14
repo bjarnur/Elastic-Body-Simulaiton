0e0641696b17d88e72ca0c04a2cd6a0447a6f95d
Bjarni <bjarniragnarg@gmail.com>
2018-05-14 14:01:53 +0200

    Initial commit

---
+++ Assets/_scripts/BodyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        //setCounterText();
+        //winText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 movement = new Vector3(0, -9, 0);
+        //rb.AddForce(movement);
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;        
+        
+        for(int i = 0; i < vertices.Length; i++)
+        {           
+            if (vertices[i].x > 0)
+            {
+                Debug.Log(vertices[i]);
+                vertices[i].x++;
+            }                
+        }
+        mesh.vertices = vertices;
+    }
+}
---
+++ Assets/_scripts/ParticleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+
+    private Rigidbody rb;
+    Vector3 speed = new Vector3(0, 0, 0);
+    Vector3 gravity = new Vector3(0, -0.1f, 0);
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //rb = GetComponent<Rigidbody>();
+        SphereCollider sc = GetComponent<SphereCollider>();
+        Vector3 pos = transform.position;
+        float radius = sc.radius;
+
+        //Apply gravitational force
+        speed += gravity * Time.deltaTime;
+        transform.position += speed;
+
+        if(pos.y < 0)
+        {
+            transform.position = new Vector3(pos.x, -pos.y / 2, pos.z);
+            speed = -(speed / 2);
+        }
+    }
+}
